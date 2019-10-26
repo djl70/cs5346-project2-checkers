a1ed1d4b14dbbb5dadc52bdecef5ac02b37f85c9
Dillon Lohr <djl70@txstate.edu>
2019-10-26 03:53:46 -0500

    Add areas for placing captured pieces

--- cs5346-project2-checkers/main.cpp
+++ cs5346-project2-checkers/main.cpp
@@ -7,7 +7,7 @@
 namespace resources
 {
 	std::unordered_map<std::string, std::string> textureFiles = {
-		{ "background", "res/background.png" },
+		{ "background", "res/checkers_game_field.png" },
 		{ "red_king", "res/checker_piece_cream_king.png" },
 		{ "black_king", "res/checker_piece_brown_king.png" },
 		{ "red_man", "res/checker_piece_cream.png" },
@@ -32,10 +32,15 @@ namespace resources
 }
 
 namespace config
-{
-	const float kScreenWidth = 1920.0f;
-	const float kScreenHeight = 1080.0f;
-	const float kSquareWidth = 100.0f;
+{ 
+	const float kScaling = 1.0f; // Controls the scaling of everything. For values other than 1, the graphics will likely be blurry.
+	const float kScreenWidth = 1920.0f * kScaling;
+	const float kScreenHeight = 1080.0f * kScaling;
+	const float kSquareWidth = 100.0f * kScaling;
+	const float kCapturedAreaOffsetFromEdge = 200.0f * kScaling;
+	const sf::Vector2f boardTopLeft{ (config::kScreenWidth - config::kSquareWidth * 8.0f) * 0.5f, (config::kScreenHeight - config::kSquareWidth * 8.0f) * 0.5f };
+	const sf::Vector2f capturedRedTopLeft{ kCapturedAreaOffsetFromEdge, (config::kScreenHeight - config::kSquareWidth * 4.0f) * 0.5f };
+	const sf::Vector2f capturedBlackTopLeft{ kScreenWidth - config::kSquareWidth * 3.0f - kCapturedAreaOffsetFromEdge, (config::kScreenHeight - config::kSquareWidth * 4.0f) * 0.5f };
 }
 
 int main()
@@ -49,8 +54,10 @@ int main()
 
 	sf::Sprite background;
 	background.setTexture(resources::textures["background"]);
+	background.setScale({ config::kScaling, config::kScaling });
 
 	sf::Sprite sprite;
+	sprite.setScale({ config::kScaling, config::kScaling });
 
 	sf::Color colorBrown(143, 67, 15, 200);
 	sf::Color colorCream(242, 174, 126, 200);
@@ -75,13 +82,13 @@ int main()
 
 		window.draw(background);
 
-		sf::Vector2f topLeft{ (config::kScreenWidth - config::kSquareWidth * 8.0f) * 0.5f, (config::kScreenHeight - config::kSquareWidth * 8.0f) * 0.5f };
+		// Draw game board
 		int i = 0;
 		for (int r = 0; r < 8; ++r)
 		{
 			for (int c = 0; c < 8; ++c)
 			{
-				sf::Vector2f position{ topLeft.x + config::kSquareWidth * c, topLeft.y + config::kSquareWidth * r };
+				sf::Vector2f position{ config::boardTopLeft.x + config::kSquareWidth * c, config::boardTopLeft.y + config::kSquareWidth * r };
 
 				rect.setPosition(position);
 				rect.setFillColor((i + r) % 2 == 0 ? colorCream : colorBrown);
@@ -119,6 +126,37 @@ int main()
 				}
 
 				++i;
+			}
+		}
+
+		// Draw captured pieces
+		for (int r = 0; r < 4; ++r)
+		{
+			for (int c = 0; c < 3; ++c)
+			{
+				sf::Vector2f redPosition{ config::capturedRedTopLeft.x + config::kSquareWidth * c, config::capturedRedTopLeft.y + config::kSquareWidth * r };
+				sf::Vector2f blackPosition{ config::capturedBlackTopLeft.x + config::kSquareWidth * c, config::capturedBlackTopLeft.y + config::kSquareWidth * r };
+
+				if (r < 2)
+				{
+					sprite.setTexture(resources::textures["red_king"]);
+					sprite.setPosition(redPosition);
+					window.draw(sprite);
+
+					sprite.setTexture(resources::textures["black_king"]);
+					sprite.setPosition(blackPosition);
+					window.draw(sprite);
+				}
+				else
+				{
+					sprite.setTexture(resources::textures["red_man"]);
+					sprite.setPosition(redPosition);
+					window.draw(sprite);
+
+					sprite.setTexture(resources::textures["black_man"]);
+					sprite.setPosition(blackPosition);
+					window.draw(sprite);
+				}
 			}
 		}
 
